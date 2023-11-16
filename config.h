6053a8f987d78df78b2cb1900390dd043a1edce6
Dave <davedanielesplana539@gmail.com>
2023-11-16 14:08:18 +0000

    Added wifi_perc function

--- config.h
+++ config.h
@@ -67,7 +67,8 @@ static const struct arg args[] = {
 	/* function format          argument */
         { battery_state, " %s",      "BAT0" },
         { battery_perc, "[%s%%]",       "BAT0" },
-        { run_command, " [%s]", "/path/to/directory"},
+        { run_command, " [%s]", "volume.sh"},
+        { wifi_perc,   " [%s]", "wlp2s0"},
         { ram_used, " [%s/", NULL},
         { ram_total, "%s]", NULL},
         { datetime, " [%s]", "%F %I:%M:%S %p"},
